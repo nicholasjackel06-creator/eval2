6ee47c9b57d5723fcc9b260a7b860f39d886db02
Nico <nicholasjackel06@gmail.com>
2025-12-19 10:12:09 +0000

    ajout de menu

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() { 
+    int nbrePlaces = 10;
+    int choix;
+    cout << "********* Evaluation *********** \n";
+    cout << "1- Entree dans le parking \n";
+    cout << "2 - Nombre de places disponibles \n";
+    cout << "3 - Sortie du parking \n";
+    cout << "4 - Quitter \n";
+    cout << "********************************* \n";
+
+
+return 0;
+}
