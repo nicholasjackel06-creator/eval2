c8260cacdc996d2a28f2c2575e2a0c508600a68b
Nico <nicholasjackel06@gmail.com>
2025-12-19 10:41:58 +0000

    ajouter le choix

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,20 @@
 using namespace std;
 int main() { 
     int nbrePlaces = 10;
-    int choix;
+    
     cout << "********* Evaluation *********** \n";
     cout << "1- Entree dans le parking \n";
     cout << "2 - Nombre de places disponibles \n";
     cout << "3 - Sortie du parking \n";
     cout << "4 - Quitter \n";
     cout << "********************************* \n";
+
+    int choix;
+        cout << "choisir une des options\n";
+        cin >> choix;
+        cout << "vous avez choisi: " << choix << endl; 
+    
+    
 
 
 return 0;
